fb1c0362f9344c1a2c69f3694eb0faf04210fffe
Gleb <130483785+GP-iOS-23@users.noreply.github.com>
2024-03-05 04:18:12 +0500

    added login/registration/storage/database managers, added subviews

---
+++ Instagram/Resources/AuthManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthManager.swift
+//  Instagram
+//
+//  Created by Глеб Поляков on 05.03.2024.
+//
+
+import Foundation
---
+++ Instagram/Resources/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  Instagram
+//
+//  Created by Глеб Поляков on 05.03.2024.
+//
+
+import Foundation
